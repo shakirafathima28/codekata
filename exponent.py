212fb7cdd7b3f5a03b6fd8a275c981fa8ada7eda
shakirafathima28 <47441084+shakirafathima28@users.noreply.github.com>
2019-02-18 15:22:49 +0530

    Update exponent.py

--- exponent.py
+++ exponent.py
@@ -1,5 +1,6 @@
-number = int(raw_input())
-exponent = int(raw_input())
+inp=raw_input().split()
+number = int(inp[0])
+exponent = int(inp[1])
 power = 1
 for i in range(1, exponent + 1):
     power = power * number
